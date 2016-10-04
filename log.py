b341c76de0034d163f25d116d6cb4013009e1bdd
Fortune Soleil <fsoleil@stuy.edu>
2016-10-04 14:20:28 -0400

    Update log.py

--- log.py
+++ log.py
@@ -1,9 +1,20 @@
 import hashlib
-
 #this goes in util
+
 def log(k, i):
-  h = hashlib.sha1(i)
+  h = hashlib.sha1(i).hexdigest()
   myfile = open("log.csv", "a")
-  myfile.write(k + "," + h.hexdigest() + "\n")
+  myfile.write(k + "," + h + "\n")
   myfile.close()
   return;
+
+def check(k, i):
+  h = hashlib.sha1(i).hexdigest()
+  myfile = open("log.csv", "r")
+  m = myfile.readlines()
+  l = []
+  for line in m:
+    l = line.split(',')
+    if (l[0] == k && l[1] == h):
+      return true;
+  return false;
