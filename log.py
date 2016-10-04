f4bb913db927cc3c16ae7e04dbf8eba0eef4d667
Fortune Soleil <fsoleil@stuy.edu>
2016-10-04 14:10:59 -0400

    Update log.py

--- log.py
+++ log.py
@@ -2,7 +2,8 @@ import hashlib
 
 #this goes in util
 def log(k, i):
+  h = hashlib.sha1(i)
   myfile = open("log.csv", "a")
-  myfile.write(k + "," + i + "\n")
+  myfile.write(k + "," + h.hexdigest() + "\n")
   myfile.close()
   return;
