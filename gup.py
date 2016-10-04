7cd556a06ca6fc0214ef4187ea94911bedbfd37e
Fortune Soleil <fsoleil@stuy.edu>
2016-10-04 14:45:55 -0400

    Update gup.py

--- gup.py
+++ gup.py
@@ -9,7 +9,7 @@ def temp():
     print "this the apperrino"
     print app
     d = request.form
-    if 'user' in d:
+    if 'user' in d.keys():
         return render_template("input.html", stuff = log.add(d['user'], d['pass']))
     else: return render_template("input.html", stuff = "")
 
@@ -19,7 +19,7 @@ def author():
     print "this the apperrino"
     print app
     if log.check(request.form['usr'], request.form['pwd']):
-        return render_template("success.html")
+        return render_template("input.html", stuff = "youer in. good jab.")
     else: return render_template("input.html", stuff = "ACC NOT FOUND.")
 
 if __name__ == "__main__":
